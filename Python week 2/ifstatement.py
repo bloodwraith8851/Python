05ff6dd21e51b3621e40bc68b4b92dde7dc1f205
bloodwraith8851 <rakeshsarkar9711@gmail.com>
2025-07-02 07:46:24 +0530

    Refactor if statement examples to remove commented-out code and improve readability

--- Python week 2/ifstatement.py
+++ Python week 2/ifstatement.py
@@ -15,9 +15,9 @@ The syntax of an if statement in Python is as follows:
 
 #example
 
-# age = 17
-# if age >= 18:
-#     print("You are eligible to vote.")
+age = 17
+if age >= 18:
+    print("You are eligible to vote.")
 
 # if-else statement
 
@@ -29,10 +29,10 @@ else:
     # code to execute if condition is false
 """
 
-# if age >= 18:
-#     print("You are eligible to vote.")
-# else:
-#     print("You are not eligible to vote.")
+if age >= 18:
+    print("You are eligible to vote.")
+else:
+    print("You are not eligible to vote.")
 
 # if-elif-else statement
 
@@ -50,17 +50,17 @@ else:
 
 #example
 
-# marks = 85
-# if marks >= 90:
-#     print("Grade: A")
-# elif marks >= 80:
-#     print("Grade: B")
-# elif marks >= 70:
-#     print("Grade: C")
-# elif marks >= 60:
-#     print("Grade: D")
-# else:
-#     print("Grade: F")
+marks = 85
+if marks >= 90:
+    print("Grade: A")
+elif marks >= 80:
+    print("Grade: B")
+elif marks >= 70:
+    print("Grade: C")
+elif marks >= 60:
+    print("Grade: D")
+else:
+    print("Grade: F")
 
 # nested if statement
 
@@ -94,13 +94,13 @@ else:
 
 # example
 
-# if age >= 18:
-#     if has_id:
-#         print("You are eligible to vote.")
-#     else:
-#         print("You need valid ID to vote.")
-# else:
-#     print("you need to be 18 or older to vote. and you need to have a valid ID.")
+if age >= 18:
+    if has_id:
+        print("You are eligible to vote.")
+    else:
+        print("You need valid ID to vote.")
+else:
+    print("you need to be 18 or older to vote. and you need to have a valid ID.")
 
 # common mistakes
 
